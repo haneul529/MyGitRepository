7e9be8c78af79c82064e8c32ea39223a0b124c7e
haneul529 <lucia00529@skuniv.ac.kr>
2023-12-14 16:26:01 +0900

    A store

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,11 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("This is Java Class [A.java]");
-<<<<<<< HEAD
 		System.out.println("Modify from master");
 		System.out.println("Modify from decB");
-=======
 		System.out.println("Modify from devC");
->>>>>>> refs/heads/devC
 	}
 }
